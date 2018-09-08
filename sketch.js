91f5d35b93904ac673e11ce83dedcc2aa71b8d98
Shivam Gupta <37578595+gilann@users.noreply.github.com>
2018-09-08 18:35:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,55 @@
+class bubble  {
+	constructor(x=50, y=100, r=100, red=10,g=10,b=100){
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.red=red;
+		this.g=g;
+		this.b=b;
+	};
+	show() {
+		noStroke();
+		fill(this.red,this.g,this.b);
+		ellipse(this.x, this.y, this.r);
+	};
+	move(){
+		this.x=this.x-random(-3,3);
+		this.y=this.y-random(-3,3);	
+	};
+	
+
+	check(bubble){
+		return(dist(bubble.x,bubble.y,this.x,this.y)<(bubble.r+this.r))
+		
+	}
+}
+var b=[50];
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-}
-var x = 0;
-var y = 0;
-var col = {
-	r : 0,
-	g: 0,
-	b: 0
-};
-function draw() {
-	while(y<=height){
-		x = 0;
-		while(x<=width){
-			col.r = random(0,255);
-			col.g = random(0,255);
-			col.b = random(0,255);
-			fill(col.r, col.g, col.b);
-			stroke(col.r, col.g, col.b);
-			ellipse(x, y, 24,24);
-			x = x+50;
-		}
-		y=y+50;
+	for(let i=0; i<50; i++){
+		b[i] = new bubble(random(width), random(height), random(10,50));
 	}
+	
+}
+
+function draw() {
+	background(0);
+	
+	b.forEach(element => {
+		element.show();
+		element.move();
+	});
+	b.forEach(element1 => {
+		b.forEach(element => {
+			if(element!=element1)
+			if(element.check(element1))
+{
+	element.red=200;
+	element1.red=200;
+	element.show();
+	element1.show();
+}			//element.move();
+		});
+	});
 }
